5c259828b9b227bf3c491ff4c3735404fa3db6b6
cynthia eddy'git congif -- global user.email cynthiaeddynyc@gmail.comgit config --global user.name cynthia <cynthiaeddynyc@gmail.com>
2020-08-25 20:08:43 -0400

    render images

--- src/components/Images.js
+++ src/components/Images.js
@@ -3,50 +3,17 @@ import ImageCard from './ImageCard';
 
 import { Card, Image } from 'semantic-ui-react';
 
-// const Images = (props) => {
-//   // imageOnClick = () => {};
-
-// 	const allimages = props.images;
-
-// 	return allimages.map((photo) => {
-// 		return <ImageCard key={photo.id} photo={photo} className="photo" data-id={photo.id} data-name={photo.name} />;
-// 	});
-// };
-
-// export default Images;
-
 class Images extends React.Component {
-	// constructor() {
-	// 	super(...arguments);
-	// 	this.state = { hide: false };
-	// }
-	// // handleChildClick() {
-	// 	this.setState({ hide: true });
-	// }
-
-	// return allimages.map((photo) => {
-	// 	return <ImageCard key={photo.id} photo={photo} className="photo" data-id={photo.id} data-name={photo.name} />;
-	// });
+	renderImages(allImages) {
+		return allImages.map((image, idx) => <ImageCard key={idx} image={image} />);
+	}
 
 	render() {
 		console.log(this.props);
 		const allImages = this.props.images;
 		console.log(allImages);
-		// const { hide } = this.state;
-		// if (hide) {
-		// 	return null;
-		// }
-		return this.props.images.map((image) => {
-			return (
-				<ImageCard
-					key={image.id}
-					image={image}
-					className="solo-card"
-					data-id={image.id}
-					// onClick={this.handleChildClick.bind(this)}
-				/>
-			);
-		});
+
+		return <div>{this.renderImages(allImages)}</div>;
 	}
 }
 
